0e860a613e7bc406f1f87fa39fa89c9907fb35a7
ReidGibson-Bingham <jsr3b0rn@gmail.com>
2023-12-16 00:57:46 -0800

    add a 404 error to the getWarehouses controller.

--- controllers/wareHouseController.js
+++ controllers/wareHouseController.js
@@ -5,25 +5,27 @@ const warehouseModel = require('../models/warehouseModel');
 
 
 const getWarehouse = async (req, res) => {
-  const id = req.params.warehouseId;
-
-  try {
-    const warehouse = await warehouseModel.getWarehouseById(id);
-
-    if (warehouse) {
-      res.status(200).json(warehouse);
-    } else {
-      res.status(404).send(`Warehouse with ID ${id} not found`);
+    const id = req.params.warehouseId;
+    try {
+        const warehouse = await warehouseModel.getWarehouseById(id);
+        if (warehouse) {
+            res.status(200).json(warehouse);
+        } else {
+            res.status(404).send(`Warehouse with ID ${id} not found`);
+        }
+    } catch (err) {
+        res.status(500).send(`Error retrieving warehouse: ${err}`);
     }
-  } catch (err) {
-    res.status(500).send(`Error retrieving warehouse: ${err}`);
-  }
 }
 
 const getWarehouses = async (_req, res) => {
     try {
         const warehouses = await warehouseModel.getWarehouses();
-        res.status(200).json(warehouses);
+        if (warehouses) {
+            res.status(200).json(warehouses);
+        } else {
+            res.status(404).send(`Warehouses not found`);
+        }
     } catch (err) {
         res.status(500).send(`Error retrieving warehouse: ${err}`);
     }
