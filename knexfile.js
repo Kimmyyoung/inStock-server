8f304f1e24bd83debfd672944017ce81d6064ee3
hykim <hiyoung.kimmy@gmail.com>
2024-01-13 13:42:28 -0800

    a

--- knexfile.js
+++ knexfile.js
@@ -6,10 +6,11 @@ require('dotenv').config();
  */
 module.exports = {
   client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  }
+  // connection: {
+  //   host: process.env.DB_HOST,
+  //   database: process.env.DB_NAME,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  // }
+  connection : process.env.CLEARDB_DATABASE_URL,
 };
